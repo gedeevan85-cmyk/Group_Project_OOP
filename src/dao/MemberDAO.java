b656e82320dccbecdbac47cd2fd755a2e94af2a8
gedee <gedee@LowKey-Vansky>
2026-07-04 16:01:00 +0800

    Merge origin/master
    # Conflicts:
    #	.gitignore

---
+++ src/dao/MemberDAO.java
@@ -0,0 +1,207 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import config.DBConnection;
+import java.sql.Connection;
+import java.util.List;
+import model.Member;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.MembershipPackage;
+/**
+ *
+ * @author gedee
+ */
+public class MemberDAO implements InterfaceDAO<Member>{
+
+    private Connection connection;
+    private MembershipPackageDAO membershipPackageDAO;
+
+    public MemberDAO() {
+        connection = DBConnection.getConnection();
+        membershipPackageDAO = new MembershipPackageDAO();
+    }
+
+    @Override
+    public void save(Member object) {
+        String sql = "INSERT INTO member "
+            + "(name, gender, birth_date, weight, height, phone, email, package_id) "
+            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, object.getName());
+            statement.setString(2, object.getGender());
+            statement.setDate(3, object.getBirthDate());
+            statement.setDouble(4, object.getWeight());
+            statement.setDouble(5, object.getHeight());
+            statement.setString(6, object.getPhone());
+            statement.setString(7, object.getEmail());
+            statement.setInt(8, object.getMembershipPackage().getPackageId());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public void update(Member object) {
+       String sql = "UPDATE member SET "
+            + "name = ?, "
+            + "gender = ?, "
+            + "birth_date = ?, "
+            + "weight = ?, "
+            + "height = ?, "
+            + "phone = ?, "
+            + "email = ?, "
+            + "package_id = ? "
+            + "WHERE member_id = ?";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, object.getName());
+            statement.setString(2, object.getGender());
+            statement.setDate(3, object.getBirthDate());
+            statement.setDouble(4, object.getWeight());
+            statement.setDouble(5, object.getHeight());
+            statement.setString(6, object.getPhone());
+            statement.setString(7, object.getEmail());
+            statement.setInt(8, object.getMembershipPackage().getPackageId());
+            statement.setInt(9, object.getMemberId());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        String sql = "DELETE FROM member WHERE member_id = ?";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @Override
+    public Member findById(int id) {
+         String sql = "SELECT * FROM member WHERE member_id = ?";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                Member member = new Member();
+
+                member.setMemberId(resultSet.getInt("member_id"));
+                member.setName(resultSet.getString("name"));
+                member.setGender(resultSet.getString("gender"));
+                member.setBirthDate(resultSet.getDate("birth_date"));
+                member.setWeight(resultSet.getDouble("weight"));
+                member.setHeight(resultSet.getDouble("height"));
+                member.setPhone(resultSet.getString("phone"));
+                member.setEmail(resultSet.getString("email"));
+
+                int packageId = resultSet.getInt("package_id");
+
+                MembershipPackage membershipPackage = membershipPackageDAO.findById(packageId);
+
+                member.setMembershipPackage(membershipPackage);
+
+                member.setCreatedAt(resultSet.getTimestamp("created_at"));
+                member.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+
+                return member;
+
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Member> findAll() {
+        List<Member> members = new ArrayList<>();
+
+        String sql = "SELECT * FROM member";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+
+                Member member = new Member();
+
+                member.setMemberId(resultSet.getInt("member_id"));
+                member.setName(resultSet.getString("name"));
+                member.setGender(resultSet.getString("gender"));
+                member.setBirthDate(resultSet.getDate("birth_date"));
+                member.setWeight(resultSet.getDouble("weight"));
+                member.setHeight(resultSet.getDouble("height"));
+                member.setPhone(resultSet.getString("phone"));
+                member.setEmail(resultSet.getString("email"));
+
+                int packageId = resultSet.getInt("package_id");
+
+                MembershipPackage membershipPackage = membershipPackageDAO.findById(packageId);
+
+                member.setMembershipPackage(membershipPackage);
+
+                member.setCreatedAt(resultSet.getTimestamp("created_at"));
+                member.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+
+                members.add(member);
+
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return members;
+    }
+}
---
+++ src/dao/TrainerDAO.java
@@ -0,0 +1,181 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import config.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Trainer;
+/**
+ *
+ * @author gedee
+ */
+public class TrainerDAO implements InterfaceDAO<Trainer>{
+
+    private Connection connection;
+
+    public TrainerDAO() {
+        connection = DBConnection.getConnection();
+    }
+
+    @Override
+    public void save(Trainer object) {
+         String sql = "INSERT INTO trainer "
+            + "(name, gender, specialization, experience_year, phone, email) "
+            + "VALUES (?, ?, ?, ?, ?, ?)";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, object.getName());
+            statement.setString(2, object.getGender());
+            statement.setString(3, object.getSpecialization());
+            statement.setInt(4, object.getExperienceYear());
+            statement.setString(5, object.getPhone());
+            statement.setString(6, object.getEmail());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public void update(Trainer object) {
+         String sql = "UPDATE trainer SET "
+            + "name = ?, "
+            + "gender = ?, "
+            + "specialization = ?, "
+            + "experience_year = ?, "
+            + "phone = ?, "
+            + "email = ? "
+            + "WHERE trainer_id = ?";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, object.getName());
+            statement.setString(2, object.getGender());
+            statement.setString(3, object.getSpecialization());
+            statement.setInt(4, object.getExperienceYear());
+            statement.setString(5, object.getPhone());
+            statement.setString(6, object.getEmail());
+            statement.setInt(7, object.getTrainerId());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+         String sql = "DELETE FROM trainer WHERE trainer_id = ?";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public Trainer findById(int id) {
+         String sql = "SELECT * FROM trainer WHERE trainer_id = ?";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                Trainer trainer = new Trainer();
+
+                trainer.setTrainerId(resultSet.getInt("trainer_id"));
+                trainer.setName(resultSet.getString("name"));
+                trainer.setGender(resultSet.getString("gender"));
+                trainer.setSpecialization(resultSet.getString("specialization"));
+                trainer.setExperienceYear(resultSet.getInt("experience_year"));
+                trainer.setPhone(resultSet.getString("phone"));
+                trainer.setEmail(resultSet.getString("email"));
+                trainer.setCreatedAt(resultSet.getTimestamp("created_at"));
+                trainer.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+
+                return trainer;
+
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Trainer> findAll() {
+        List<Trainer> trainers = new ArrayList<>();
+
+        String sql = "SELECT * FROM trainer";
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+
+                Trainer trainer = new Trainer();
+
+                trainer.setTrainerId(resultSet.getInt("trainer_id"));
+                trainer.setName(resultSet.getString("name"));
+                trainer.setGender(resultSet.getString("gender"));
+                trainer.setSpecialization(resultSet.getString("specialization"));
+                trainer.setExperienceYear(resultSet.getInt("experience_year"));
+                trainer.setPhone(resultSet.getString("phone"));
+                trainer.setEmail(resultSet.getString("email"));
+                trainer.setCreatedAt(resultSet.getTimestamp("created_at"));
+                trainer.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+
+                trainers.add(trainer);
+
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return trainers;
+    }
+}
